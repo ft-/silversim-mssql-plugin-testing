a90f1f4aeca94213507ebac081a8e6c01a033d18
ft <ft@noemail>
2017-10-17 23:54:28 +0200

    removed wrong tables from remove parcel

--- SilverSim/Database.MsSql/SimulationData/MsSqlSimulationDataStorage.ParcelStorage.cs
+++ SilverSim/Database.MsSql/SimulationData/MsSqlSimulationDataStorage.ParcelStorage.cs
@@ -119,8 +119,6 @@ namespace SilverSim.Database.MsSql.SimulationData
         private static readonly string[] ParcelTables = new string[]
         {
             "parcelexperiences",
-            "regionexperiences",
-            "regiontrustedexperiences",
             "parcelaccesswhitelist",
             "parcelaccessblacklist",
             "parcellandpasslist"
