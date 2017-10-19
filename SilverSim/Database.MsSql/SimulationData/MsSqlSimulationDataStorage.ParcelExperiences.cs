8e9f1b3db95dd2ee1503623e1c62c22afac3d868
ft <ft@noemail>
2017-10-19 23:13:02 +0200

    corrected TryGetValue in ParcelExperiences

--- SilverSim/Database.MsSql/SimulationData/MsSqlSimulationDataStorage.ParcelExperiences.cs
+++ SilverSim/Database.MsSql/SimulationData/MsSqlSimulationDataStorage.ParcelExperiences.cs
@@ -127,7 +127,8 @@ namespace SilverSim.Database.MsSql.SimulationData
                 {
                     ["RegionID"] = entry.RegionID,
                     ["ParcelID"] = entry.ParcelID,
-                    ["ExperienceID"] = entry.ExperienceID
+                    ["ExperienceID"] = entry.ExperienceID,
+                    ["IsAllowed"] = entry.IsAllowed
                 };
                 connection.ReplaceInto("parcelexperiences", data, ParcelExperienceKeys);
             }
@@ -139,7 +140,7 @@ namespace SilverSim.Database.MsSql.SimulationData
             {
                 connection.Open();
                 /* we use a specific implementation to reduce the result set here */
-                using (var cmd = new SqlCommand("SELECT ExperienceID FROM parcelexperiences WHERE RegionID = @regionid AND ParcelID = @parcelid AND ExperienceID LIKE @experienceid", connection))
+                using (var cmd = new SqlCommand("SELECT IsAllowed FROM parcelexperiences WHERE RegionID = @regionid AND ParcelID = @parcelid AND ExperienceID LIKE @experienceid", connection))
                 {
                     cmd.Parameters.AddParameter("@regionid", regionID);
                     cmd.Parameters.AddParameter("@parcelid", parcelID);
@@ -150,9 +151,9 @@ namespace SilverSim.Database.MsSql.SimulationData
                         {
                             entry = new ParcelExperienceEntry
                             {
-                                RegionID = reader.GetUUID("RegionID"),
-                                ParcelID = reader.GetUUID("ParcelID"),
-                                ExperienceID = reader.GetUUID("ExperienceID"),
+                                RegionID = regionID,
+                                ParcelID = parcelID,
+                                ExperienceID = experienceID,
                                 IsAllowed = (bool)reader["IsAllowed"]
                             };
                             return true;
