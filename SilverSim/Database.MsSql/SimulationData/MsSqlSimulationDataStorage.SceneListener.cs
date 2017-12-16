67ff20e2aed581e67895aa5aa4532e576216ef72
ft <ft@noemail>
2017-12-16 10:49:58 +0100

    add UnSitTarget storage

--- SilverSim/Database.MsSql/SimulationData/MsSqlSimulationDataStorage.SceneListener.cs
+++ SilverSim/Database.MsSql/SimulationData/MsSqlSimulationDataStorage.SceneListener.cs
@@ -496,7 +496,10 @@ namespace SilverSim.Database.MsSql.SimulationData
                     ["ExtendedMeshData"] = objpart.ExtendedMesh.DbSerialization,
                     ["IsSitTargetActive"] = objpart.IsSitTargetActive,
                     ["IsScriptedSitOnly"] = objpart.IsScriptedSitOnly,
-                    ["AllowUnsit"] = objpart.AllowUnsit
+                    ["AllowUnsit"] = objpart.AllowUnsit,
+                    ["IsUnSitTargetActive"] = objpart.IsUnSitTargetActive,
+                    ["UnSitTargetOffset"] = objpart.UnSitTargetOffset,
+                    ["UnSitTargetOrientation"] = objpart.UnSitTargetOrientation
                 };
                 using (var ms = new MemoryStream())
                 {
